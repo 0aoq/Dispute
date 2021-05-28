5fe92d1796b26807a10f0edbba64bf1879e20d64
0aoq <73413330+0aoq@users.noreply.github.com>
2021-05-28 11:41:59 -0400

    Update data.js

--- public/src/firebase/firestore/data.js
+++ public/src/firebase/firestore/data.js
@@ -22,11 +22,6 @@ function switch_server(server) {
     window.location.reload()
 }
 
-function switch_dm(dm) {
-    window.localStorage.setItem("current_dm", dm)
-    window.location.reload()
-}
-
 auth.onAuthStateChanged((user) => {
     if (user) {
         db.collection("users").doc(user.uid).get().then((doc) => {
@@ -133,12 +128,6 @@ auth.onAuthStateChanged((user) => {
             })
         })
 
-        /*
-
-        * SERVERS/DMs
-
-        */
-
         if (!window.localStorage.getItem("current_dm") != "") {
             if (document.getElementById("page").innerHTML == "servers" && window.localStorage.getItem("current_channel") && window.localStorage.getItem("current_server")) {
                 document.getElementById("server_info__channel_name").innerText = "#" + window.localStorage.getItem("current_channel").split("!:DISPUTE_CHANNEL::GET::!?")[0]
@@ -176,16 +165,6 @@ auth.onAuthStateChanged((user) => {
                             let code = datapoint.split("!:DISPUTE_CHANNEL::GET::!?")[1]
                             let type = datapoint.split("!:DISPUTE_CHANNEL_TYPE::GET::!?")[1]
 
-                            /* 
-                               TODO:
-                                Check the set channel type, create a voice tab if type == "voice"
-                                Create a normal tab if type == "chat"
-    
-                               DONE:
-                                Create channel type base,
-                                Begin adding channel icon to channel button
-                            */
-
                             if (!document.getElementById(channel_name + "!:DISPUTE_CHANNEL::GET::!?" + code && datapoint.split("!:DISPUTE_CHANNEL::GET::!?")[2] == null)) {
                                 if (type == "Chat" || type == null) {
                                     document.getElementById("channels").insertAdjacentHTML("beforeend", `
@@ -211,21 +190,6 @@ auth.onAuthStateChanged((user) => {
                             }
                         }
                     })
-
-                /* if (window.localStorage.getItem("current_channel") != "Dispute Home") { // each channel has docs for each user, check if the user already has one
-                    db.collection(`servers/${window.localStorage.getItem("current_server")}/${window.localStorage.getItem("current_channel")}`).doc(user.uid).get().then((doc) => {
-                        if (doc.exists) {
-                            log("Currently signed in user already has a profile in this channel.", console_styles.success)
-                        } else { // if not, make one
-                            log("A profile has been created for the currently signed in user.", console_styles.success)
-                            db.collection(`servers/${window.localStorage.getItem("current_server")}/${window.localStorage.getItem("current_channel")}`).doc(user.uid).set({
-                                name: user.displayName,
-                                can_write: false,
-                                sent: []
-                            })
-                        }
-                    })
-                } */
 
                 db.collection(`servers/${window.localStorage.getItem("current_server")}/${window.localStorage.getItem("current_channel")}`)
                     .onSnapshot((querySnapshot) => { // Add real time support
@@ -404,172 +368,7 @@ auth.onAuthStateChanged((user) => {
                 })
             }
         } else {
-            log("The current user is viewing their DMs.")
-
-            if (window.localStorage.getItem("current_dm").split(", ")[2] == user.displayName) {
-                document.getElementById("server_info__channel_name").innerText = window.localStorage.getItem("current_dm").split(", ")[3]
-                document.getElementById("server_name").innerText = "Direct Messages"
-                document.getElementById("channel__name__2").innerText = window.localStorage.getItem("current_dm").split(", ")[3]
-                document.title = window.localStorage.getItem("current_dm").split(", ")[3] + " - On Dispute"
-
-                document.getElementById("messagebox").style.display = "block"
-            } else if (window.localStorage.getItem("current_dm").split(", ")[2] != "Home") {
-                document.getElementById("server_info__channel_name").innerText = window.localStorage.getItem("current_dm").split(", ")[2]
-                document.getElementById("server_name").innerText = "Direct Messages"
-                document.getElementById("channel__name__2").innerText = window.localStorage.getItem("current_dm").split(", ")[2]
-                document.title = window.localStorage.getItem("current_dm").split(", ")[2] + " - On Dispute"
-
-                document.getElementById("messagebox").style.display = "block"
-            } else {
-                document.title = "Viewing direct messages - On Dispute"
-            }
-
-            // Load join modal button
-            document.getElementById("channels").insertAdjacentHTML("beforeend", `
-                <a style="display: flex;" class="channel" onclick="open_dm_modal()">
-                    <i data-feather="plus-square" style="margin-right: 10px;"></i> New DM
-                </a> 
-            `)
-
-            feather.replace()
-
-            db.collection("directMessages")
-                .onSnapshot((querySnapshot) => {
-                    querySnapshot.forEach((doc) => {
-                        document.getElementById("channels").innerHTML = ""
-                        document.getElementById("channels").insertAdjacentHTML("beforeend", `
-                            <a style="display: flex;" class="channel" onclick="open_modal('dm_modal')">
-                                <i data-feather="plus-square" style="margin-right: 10px;"></i> New DM
-                            </a> 
-                        `)
-
-                        let dm_user_1 = doc.id.split(", ")[0]
-
-                        let dm_userName_1 = doc.id.split(", ")[2]
-                        let dm_userName_2 = doc.id.split(", ")[3]
-
-                        if (user.uid == dm_user_1) {
-                            document.getElementById("channels").insertAdjacentHTML("beforeend", `
-                            <a style="display: flex;" class="channel" onclick="switch_dm('${doc.id}')">
-                                <i data-feather="user" style="margin-right: 10px;"></i> ${dm_userName_2}
-                            </a> 
-                        `)
-                        } else {
-                            document.getElementById("channels").insertAdjacentHTML("beforeend", `
-                            <a style="display: flex;" class="channel" onclick="switch_dm('${doc.id}')">
-                                <i data-feather="user" style="margin-right: 10px;"></i> ${dm_userName_1}
-                            </a> 
-                        `)
-                        }
-
-                        feather.replace()
-                    })
-                })
-
-            db.collection("directMessages").doc(window.localStorage.getItem("current_dm"))
-                .onSnapshot((doc) => {
-                    let data = doc.data()
-
-                    for (datapoint of data.sent) {
-                        let msg = datapoint.content
-                        let code = datapoint.token
-                        let int = datapoint.order
-                        if (!document.getElementById(datapoint.author + ":" + code)) {
-                            // fix md
-                            msg = msg.replaceAll('"', "&quot;") // replace quotes because they used to break stuff
-
-                            msg = msg.replaceAll('# ', "")
-                            msg = msg.replaceAll('## ', "")
-                            msg = msg.replaceAll('### ', "")
-                            msg = msg.replaceAll('#### ', "")
-                            msg = msg.replaceAll('##### ', "")
-
-                            // TODO: delete_msg(data.uid, codes)
-
-                            document.getElementById("msgs").insertAdjacentHTML("beforeend", `
-        
-                                <li class="card message" style="margin-top: 5px; width: 99%; order: ${int};" id='${datapoint.author + ":" + code}'>
-                                    <h5 style="display: inline; user-select: none;">${datapoint.author}</h5>
-                                    <span style="margin-left: 20px;">${marked(msg)}</span>
-                                </li>
-                        
-                            `)
-
-                            let links = document.querySelectorAll("#msgs li a")
-                            for (var i = 0, len = links.length; i < len; i++) {
-                                if (!links[i].classList.contains("btn")) {
-                                    links[i].classList.add("btn")
-                                }
-                            }
-
-                            let p = document.querySelectorAll("#msgs li p")
-                            for (var i = 0, len = p.length; i < len; i++) {
-                                p[i].style.display = "inline"
-                            }
-
-                            $("#msgs").animate({ scrollTop: $('#msgs').prop("scrollHeight") }, 1000);
-                        }
-                    }
-                })
-
-            document.getElementById("dm_form").addEventListener('submit', e => {
-                e.preventDefault()
-
-                db.collection("users")
-                    .onSnapshot((querySnapshot) => {
-                        querySnapshot.forEach((userProfile) => {
-                            let userData = userProfile.data()
-                            if (userData.userId == document.getElementById("dm_form").userid.value && userProfile.id != user.uid) { // check for the user id
-                                db.collection("directMessages").doc(`${userProfile.id}, ${user.uid}, ${userData.name}, ${user.displayName}`).set({
-                                    profiles: [
-                                        userProfile.id,
-                                        user.uid
-                                    ],
-                                    sent: [],
-                                    total_msgs: 0
-                                }).then(() => {
-                                    log("Created DM.", console_styles.success)
-                                }).catch(error => {
-                                    log(error, console_styles.warning)
-                                })
-                            }
-                        })
-                    })
-            })
-
-            let user_msgs_allowed = true
-            message_form.addEventListener('submit', e => {
-                e.preventDefault()
-
-                if (message_form.msg.value != "" && message_form.msg.value != " " && message_form.msg.value != null && message_form.msg.value && user_msgs_allowed == true) {
-                    db.collection("directMessages").doc(window.localStorage.getItem("current_dm")).get().then((doc) => {
-                        let data = doc.data()
-
-                        user_msgs_allowed = false
-
-                        data.sent.push({ // message strucutre
-                            author: user.displayName,
-                            content: message_form.msg.value,
-                            token: getString(12),
-                            order: data.total_msgs
-                        })
-
-                        db.collection("directMessages").doc(window.localStorage.getItem("current_dm")).set(data).then(() => {
-                            log("Written message to DM.", console_styles.success)
-                        }).catch((error) => {
-                            console.log(error)
-                        })
-
-                        setTimeout(() => {
-                            user_msgs_allowed = true
-                        }, 1000);
-                    });
-
-                    setTimeout(() => {
-                        message_form.reset()
-                    }, 500);
-                }
-            })
+            log("Cannot load server - User is viewing their DMs.", console_styles.warning)
         }
 
         console.log("\n\n==========================================\n\n")
